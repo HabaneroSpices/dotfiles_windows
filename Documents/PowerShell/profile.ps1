058853b068fac13c6777f12c43ef6834ee8c4ed6
Habanero <habanerospices@gmail.com>
2023-07-28 21:15:17 +0200

    Update Documents/PowerShell/profile.ps1

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -29,3 +29,36 @@ function touch
     Set-Content -Path ($args[0]) -Value ($null)
   }
 }
+function Verify-Elevated
+{
+  # Get the ID and security principal of the current user account
+  $myIdentity=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+  $myPrincipal=new-object System.Security.Principal.WindowsPrincipal($myIdentity)
+  # Check to see if we are currently running "as Administrator"
+  return $myPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+# Reload the Shell
+function Reload-Powershell
+{
+  $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+  $newProcess.Arguments = "-nologo";
+  [System.Diagnostics.Process]::Start($newProcess);
+  exit
+}
+# Reload the $env object from the registry
+function Refresh-Environment
+{
+  $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
+  'HKCU:\Environment'
+
+  $locations | ForEach-Object {
+    $k = Get-Item $_
+    $k.GetValueNames() | ForEach-Object {
+      $name  = $_
+      $value = $k.GetValue($_)
+      Set-Item -Path Env:\$name -Value $value
+    }
+  }
+
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
